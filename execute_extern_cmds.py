0c2da4a5f89e916a2486e40f48df0c54a55239d3
Julie <julie.kleines@gmail.com>
2020-05-25 22:25:33 +0200

    add file to execute extern commands

---
+++ execute_extern_cmds.py
@@ -0,0 +1,22 @@
+import os
+
+# toutes les fonctions utiles
+# par exemple pour l'affichage de differents graphes
+
+def compile():
+	command="gcc main.cpp -o main.out"
+	print(command)
+	return os.system(command)
+
+def run_cpp_ea(nb_children, nb_generations, file_path):
+    command="./main.out "+str(nb_children)+" "+str(nb_generations)+" "+str(file_path)
+    print(command)
+    return os.system(command)
+
+
+# permet de generer le main.out
+compile()
+
+# MODIFIER CETTE LIGNE EN METTANT LES ARGUMENTS DE VOTRE CHOIX
+run_cpp_ea(4,5,"gene.csv")
+
--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
+#include <stdio.h>
 #include "construction.h"
-#include <iostream>
 
+int main(int argc, char *argv[]) {
 
+   printf("Hello, Julie!");
 
-int main(){
+   // pour s'assurer que tout a bien ete pris correctement en argument
+   // argv[0] holds the name of the program.
+   // argv[1] points to the first command line argument etc.
+   int counter;
+   printf("\nProgram Name Is: %s",argv[0]);
+   if(argc==1) 
+        printf("\nNo Extra Command Line Argument Passed Other Than Program Name"); 
+    if(argc>=2) 
+    { 
+        printf("\nNumber Of Arguments Passed: %d",argc); 
+        printf("\n----Following Are The Command Line Arguments Passed----"); 
+        for(counter=0;counter<argc;counter++) 
+            printf("\nargv[%d]: %s",counter,argv[counter]); 
+    }
+
+    // debut des objets
+    construction obj(0,0,"gene.csv");
+	  obj.dataManage();
 	
-	construction obj(0,0,"gene.csv");
-	obj.dataManage();
-	return 0;
 
-};
+   return 0;
+}
