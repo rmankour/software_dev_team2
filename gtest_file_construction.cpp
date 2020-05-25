597c9ebc7c8e1e97cf8c2a1dcb8c06440ff76ffd
rmankour <rafik.mankour@insa-lyon.fr>
2020-05-25 15:11:17 +0200

    gtest actualisé

--- gtest_file_construction.cpp
+++ gtest_file_construction.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "construction.cpp"
+#include "fonction.cpp"
 #include <string>
 #include <iostream>
 
@@ -7,13 +8,19 @@
 /* run make clean to get rid of intermediate files */
 
 // Basic GTest
-TEST(GTestTests, test_dataManage_lectureCaseTab) 
+
+TEST(GTestTests, basic_test) {
+
+    EXPECT_TRUE(true);
+}
+
+TEST(GTestTests, test_dataManage_lectureCaseTab)
 {
 	construction obj(0,0,"gene.csv");
 	obj.dataManage();
 	std::string chartest = "";
 	std::string expectedchar = "110101001011011001000011010011010100111111101011100001101001101111010111101011111011110011111001100100111111110100100011";
-	
+
 	for(size_t j=0; j<obj.get_nbcol(); j++)
 	{
 		if ((obj.lectureCaseTab(obj.get_nblig()-1,j)==false))
@@ -24,4 +31,11 @@ TEST(GTestTests, test_dataManage_lectureCaseTab)
 	}
 	EXPECT_EQ(chartest,expectedchar);
 
+}
+
+fonction f1(20);
+TEST(GTestTests, fonctionConstructor_test){
+	for(int i=0 ; i<20 ; i++){
+		EXPECT_TRUE(f1.getRankYN()[i] == 0 || f1.getRankYN()[i] == 1);
+	}
 }
