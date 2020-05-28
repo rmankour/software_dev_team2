eed0d7a5c500024a7f2e88190735c284020d6223
rmankour <rafik.mankour@insa-lyon.fr>
2020-05-28 12:14:59 +0200

    Ajouté les méthodes generation, theCycleOfLife et les modifs nécessaires au constructeur, le tout en commentaire en attendant de pouvoir tester

--- construction.h
+++ construction.h
@@ -7,7 +7,7 @@ class construction
     construction(const int gen, const int ind, const std::string adress);
     void dataManage();
     bool lectureCaseTab(size_t lig, size_t col);
-    fonction* generation(bool popuInitiale);
+    void generation();
     fonction SSE(const fonction* &tab_fonctions); // reçoit un tableau de formule et retourne la meilleure d'entre elles (en prenant aussi en compte la myFormule actuelle)
     void theCycleOfLife(); // prend tous les paramètres donnés par l'utilisateur et réalise la boucle de calculs nécessaire pour aboutir à la myFormule finale
     size_t get_nblig();
@@ -18,6 +18,10 @@ class construction
     std::string adresse_; // Adresse du fichier .CSV fournie par l'utilisateur
     int numChildren_; // Nombre de d'individus par génération, indiqué par l'utilisateur ou valeur par défaut
     int numGenerations_; // Nombre de générations à réaliser, indiqué par l'utilisateur ou valeur par défaut
+    int compteurFormules;
+    int *tab_positions; // stocke la position des mutations
+    int *tab_type; // stocke le type de mutations
+    int *tab_rang // stocke le rang avec le lequel se fait un échange en cas d'interversion (-1 si pas d'interversion)
     bool** tab2d_; // Contient dans un tableau en 2D les données fournies par l'utilisateur
     size_t nb_ligtab2D_;
     size_t nb_coltab2D_;
