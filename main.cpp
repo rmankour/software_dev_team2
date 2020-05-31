9216daf9bb3454275d5b69ed738ce1689e384947
Julie <julie.kleines@gmail.com>
2020-05-31 13:51:53 +0200

    corrected order of arguments in generation git add main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 // ***************** REMARQUE : les lignes qu'il y a avait dans le main.cpp ne permettaient pas de compiler donc elles sont en commentaires
 
 int main(int argc, char *argv[]) {
-
+    /*
    printf("Hello, this is the beginning !");
 
    // pour s'assurer que tout a bien ete pris correctement en argument
@@ -25,16 +25,16 @@ int main(int argc, char *argv[]) {
         for(counter=0;counter<argc;counter++)
             printf("\nargv[%d]: %s",counter,argv[counter]);
     }
-
+    */
     // on met les arguments dans des variables
     int nb_children = std::atoi(argv[1]);  // converti notre char en int 
     int nb_generation = std::atoi(argv[2]) ; // converti notre char en int
     std::string adresse_tab = argv[3]; // constructeur de string converti directement un char en int sans soucis.
 
-
+    
     // debut des objets
     
-    construction obj(nb_children,nb_generation,adresse_tab);
+    construction obj(nb_generation,nb_children,adresse_tab);
 
     obj.generation();
 	  //obj.dataManage();
